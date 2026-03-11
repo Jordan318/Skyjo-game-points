de4d9c35f07e1376a5264cf8103d289ddc476346
Jordan318 <jordan.desevaux@gmail.com>
2026-03-10 20:42:56 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'skyjo-v1';
+const ASSETS = [
+  '/Skyjo-game-points/',
+  '/Skyjo-game-points/index.html',
+  '/Skyjo-game-points/manifest.json',
+  '/Skyjo-game-points/icons/icon-192.png',
+  '/Skyjo-game-points/icons/icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
